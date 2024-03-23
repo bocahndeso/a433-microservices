49422530c2bd17f893ebef969babce53e872c718
bocahndeso <bocahndeso@gmail.com>
2024-03-23 05:25:51 +0000

    membuat file build_push_image_karsajobs.sh

---
+++ build_push_image_kasajobs.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#Build image dari Dockerfile yang telah disediakan
+docker build -t ghcr.io/bocahndeso/karsajobs:latest .
+
+#Login ke Github Packages menggunakan Personal Access Token yang disimpan di environment variabel
+echo "$GITHUB_TOKEN" | docker login ghcr.io -u bocahndeso --password-stdin
+
+#unggah image ke Github Packages
+docker push ghcr.io/bocahndeso/karsajobs:latest
